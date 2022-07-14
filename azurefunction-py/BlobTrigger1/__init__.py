021b509718505383b599c1330d63799b059ad7af
anotherRedbeard <andrewredman@redbeard-mbp.lan>
2022-07-14 13:16:51 -0500

    remove code

--- azurefunction-py/BlobTrigger1/__init__.py
+++ azurefunction-py/BlobTrigger1/__init__.py
@@ -5,5 +5,5 @@ import azure.functions as func
 
 def main(myblob: func.InputStream):
     logging.info(f"Python blob trigger function processed blob \n"
-                 f"Name new code: {myblob.name}\n"
+                 f"Name: {myblob.name}\n"
                  f"Blob Size: {myblob.length} bytes")
