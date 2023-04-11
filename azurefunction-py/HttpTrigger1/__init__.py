740fca941426496fe0d402ad3e35fc69d60651fa
anotherRedbeard <andrewredman@redbeard-mbp.lan>
2023-04-11 11:24:20 -0500

    small test

--- azurefunction-py/HttpTrigger1/__init__.py
+++ azurefunction-py/HttpTrigger1/__init__.py
@@ -16,7 +16,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully. test")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
