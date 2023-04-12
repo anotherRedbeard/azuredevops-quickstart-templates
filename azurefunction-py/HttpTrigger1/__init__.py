2d3268bd17c6ea8e2fe6ba817e08d95148c04e12
anotherRedbeard <andrewredman@redbeard-mbp.lan>
2023-04-11 19:25:28 -0500

    add environment variable

--- azurefunction-py/HttpTrigger1/__init__.py
+++ azurefunction-py/HttpTrigger1/__init__.py
@@ -1,10 +1,12 @@
 import logging
+import os
 
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    env_setting = os.environ['EnvSetting']
+    logging.info(f'Python HTTP trigger function processed a request for {env_setting} environment.')
 
     name = req.params.get('name')
     if not name:
@@ -16,7 +18,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully. test")
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully in {env_setting} env.")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
